62e4c68eacddccc5cf70022d83a89b84e9ccd0d5
Eduardo Salgado <71921174+j-e-s-h@users.noreply.github.com>
2022-05-23 14:38:42 -0500

    Initial commit.

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import re
+
+project_slug = "{{ cookiecutter.project_slug }}"
+
+
+MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
+
+ERROR_COLOR = "\x1b[31m"
+MESSAGE_COLOR = "\x1b[34m"
+RESET_ALL = "\x1b[0m"
+
+
+if not re.match(MODULE_REGEX, module_name):
+    print(f'{ERROR_COLOR}ERROR: %s is not a valid name.{RESET_ALL}')
+
+    sys.exit(1)
+
+print(f"{MESSAGE_COLOR}Let's do it! You're are going to create something awesome!")
+print(f"Creating project at { os.getcwd() }{RESET_ALL}")
